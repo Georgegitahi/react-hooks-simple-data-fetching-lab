c1eb1ec51cc85e22147b3530cc93430643cb7dd4
Georgegitahi <petergeorgembugua724@gmail.com>
2024-05-01 18:07:41 +0300

    lab complete

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,74 @@
 // create your App component here
+import React, { useState, useEffect} from 'react'
+
+//import React from 'react'
+
+ function App() {
+    const [dogPic,setDogPic]= useState(null);
+
+
+    useEffect(() => {
+        fetch('https://dog.ceo/api/breeds/image/random')
+        .then((response) => response.json())
+        .then((data) => {
+            setDogPic(data.message);
+        });
+
+    }, []);
+    
+    if (!dogPic) return <p>Loading...</p>;
+
+    return <img src={dogPic} alt="A Random Dog" />;
+  
+}
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// useEffect(() => {
+//     fetch('https://dog.ceo/api/breeds/image/random')
+//     .then((response) => response.json())
+//     // .then((json) => console.log(json));
+//     .then(setData)
+
+// }, [])
+
